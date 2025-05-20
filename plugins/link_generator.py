295253d644639f1f14683279a86bbc6d638216e7
Gumas <148417921+Opmasterod@users.noreply.github.com>
2025-05-20 12:10:57 +0530

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -5,7 +5,7 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from bot import Bot
 from pyrogram.types import ReplyKeyboardMarkup, ReplyKeyboardRemove
 from asyncio import TimeoutError
-from helper_func import encode, get_message_id, admin
+from helper_func import encode, get_message_id, admin, encode_link
 
 @Bot.on_message(filters.private & admin & filters.command('batch'))
 async def batch(client: Client, message: Message):
@@ -102,3 +102,39 @@ async def custom_batch(client: Client, message: Message):
 
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
     await message.reply(f"<b>Here is your custom batch link:</b>\n\n{link}", reply_markup=reply_markup)
+
+@Bot.on_message(filters.private & filters.user(ADMINS) & filters.command('nbatch'))
+async def new_batch(client: Client, message: Message):
+    while True:
+        try:
+            first_message = await client.ask(text="Forward the First Message from DB Channel (with Quotes)..\n\nor Send the DB Channel Post Link", chat_id=message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
+        except:
+            return
+        f_msg_id = await get_message_id(client, first_message)
+        if f_msg_id:
+            break
+        else:
+            await first_message.reply("❌ Error\n\nthis Forwarded Post is not from my DB Channel or this Link is taken from DB Channel", quote=True)
+            continue
+
+    while True:
+        try:
+            second_message = await client.ask(text="Forward the Last Message from DB Channel (with Quotes)..\nor Send the DB Channel Post link", chat_id=message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
+        except:
+            return
+        s_msg_id = await get_message_id(client, second_message)
+        if s_msg_id:
+            break
+        else:
+            await second_message.reply("❌ Error\n\nthis Forwarded Post is not from my DB Channel or this Link is taken from DB Channel", quote=True)
+            continue
+
+    # Use encode_link to generate the new format: get-HACKHEIST_{f_msg_id*10}-{s_msg_id*10}
+    link = await encode_link(f_msg_id, s_msg_id, client.db_channel.id)
+    if WEBSITE_URL_MODE == True:
+        # Extract the base64 part and format as website URL
+        base64_string = link.split("start=")[1]
+        link = f"{WEBSITE_URL}?HACKHEIST={base64_string}"
+
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
+    await second_message.reply_text(f"<b>Here is your new batch link</b>\n\n<code>{link}</code>", quote=True, reply_markup=reply_markup)
