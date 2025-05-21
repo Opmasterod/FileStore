9d310610591c24c09d23213619d687b7e20c3a10
Gumas <148417921+Opmasterod@users.noreply.github.com>
2025-05-21 05:53:45 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -189,9 +189,10 @@ async def start_command(client: Client, message: Message):
             await message.reply_text(f"Failed to decode string: {str(e)}")
             return
 
-        temp_msg = await message.reply(f"Wait A Second...",
-                                      message_effect_id=5104841245755180586
-                                      )
+        temp_msg = await message.reply(
+            f"Wait A Second...",
+            message_effect_id=5104841245755180586
+            )
         )
         try:
             messages = await get_messages(client, ids)
